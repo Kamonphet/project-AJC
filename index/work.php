f2eb1e585e5b84de93a21811102d9f1d4880bc4b
Kamonphet <62428925+Kamonphet@users.noreply.github.com>
2021-11-28 23:29:28 +0700

    Update work.php

--- index/work.php
+++ index/work.php
@@ -13,7 +13,7 @@ use App\model\Homework;
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>งานทั้งหมด</title>
+    <title>Boonraksa System</title>
     <link href="../css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <link href="../css/colors_bt5.css" rel="stylesheet">
 </head>
