41b49d412394a4c596ed5cef6f717d23e6105e75
Kamonphet <62428925+Kamonphet@users.noreply.github.com>
2021-11-28 23:29:02 +0700

    Update creatework.php

--- index/creatework.php
+++ index/creatework.php
@@ -20,7 +20,7 @@ if ($_REQUEST['action']=='edit') {
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>เพิ่มงาน</title>
+    <title>Boonraksa System</title>
     <link href="../css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <link href="../css/colors_bt5.css" rel="stylesheet">
 </head>
